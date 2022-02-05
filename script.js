4e00ff0c924928cadfd8e856a68794a6c8ea2650
José Rodolpho <j.rodolpho29@hotmail.com>
2022-02-04 22:37:43 -0300

    Criação da animação de virada de carta

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let numeroCartas = 0
+let gifs = ["bobrossparrot.gif", "bobrossparrot.gif", "explodyparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "metalparrot.gif", "revertitparrot.gif", "revertitparrot.gif", "tripletsparrot.gif", "tripletsparrot.gif", "unicornparrot.gif", "unicornparrot.gif"]
 
 function verificarCarta() {
 for(numeroCartas;numeroCartas < 4 || numeroCartas > 14|| numeroCartas & 2 !== 0;) {
@@ -6,12 +7,19 @@ for(numeroCartas;numeroCartas < 4 || numeroCartas > 14|| numeroCartas & 2 !== 0;
 }
 }
 
-
 function colocarCartas() {
     const imagem = document.querySelector(".imagens")
     for(let i = 0; i < numeroCartas;i++){
-        imagem.innerHTML += "<div class='carta'><img src='Media/front.png'></div>"
+        imagem.innerHTML += `<div class="carta n${i}" onclick= virarCarta(this)>
+        <img class="foto n${i} frente" src='Media/front.png' />
+        <img class="foto n${i}  verso" src='Media/${gifs[i]}' />
+        </div>`
     }
+}
+
+function virarCarta(divSelecionada){
+    divSelecionada.querySelector(".frente").style.transform = "rotateY(-180deg)"
+    divSelecionada.querySelector(".verso").style.transform = "rotateY(0deg)"
 }
 
 verificarCarta()
